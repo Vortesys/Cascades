e5150e1058ccec415a363eaf254820883cd81e3a
Brady McDermott <freedom7341@hotmail.com>
2024-01-14 19:54:10 -0700

    "boilerplate"

---
+++ src/ntstyle.c
@@ -0,0 +1,8 @@
+/* * * * * * * *\
+	NTSTYLE.C -
+		Copyright © 2023 Brady McDermott, Vortesys
+	DESCRIPTION -
+		NT Style's main file.
+	LICENSE INFORMATION -
+		MIT License, see LICENSE.txt in the root folder
+ \* * * * * * * */
